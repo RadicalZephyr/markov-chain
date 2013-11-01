0410ba021872b022a8effe267a284661f46f3881
Geoff Shannon <earthlingzephyr@gmail.com>
2013-11-01 14:00:41 -0700

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject markov-chain "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A markov text generator"
+  :url "https://github.com/ezephyr/markov-chain"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
